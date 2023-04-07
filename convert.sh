5e09869481bcb6b3c5477a8122fb9d5371bf4a9c
Laszlo David <laszlo.david@gmail.com>
2023-04-07 21:56:59 +0200

    fade out, fade in shell script

--- convert.sh
+++ convert.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./convert --fade_out --input input.kla --output output.kla --source_output fadeout.asm --sprite_from sprite_colors_from.txt --sprite_to sprite_colors_to.txt  --color 01
+./convert --fade_out --input base_kicsihud320.kla --output image_part6.kla --source_output fadeout_part6.asm --sprite_from sprite_colors_from.txt --sprite_to sprite_colors_to.txt  --color 01
+./convert --fade_in --input base_exp_10_320.kla --output image_part7.kla --source_output fadein_part7.asm --color 01
